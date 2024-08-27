c5b815a4e5b7b0cc1909820690f41580f32cac96
Abdul Majeed Kanoor <abdulkanoor@gmail.com>
2024-08-28 01:11:19 +0530

    Initail Commit

---
+++ src/main/java/com/dailycodebuffer/springbootupload/service/AttachmentService.java
@@ -0,0 +1,13 @@
+package com.dailycodebuffer.springbootupload.service;
+
+import com.dailycodebuffer.springbootupload.entity.Attachment;
+import org.springframework.web.multipart.MultipartFile;
+
+
+public interface AttachmentService {
+
+
+    Attachment saveAttachment(MultipartFile file) throws Exception;
+
+    Attachment getAttachment(String fileId) throws Exception;
+}
